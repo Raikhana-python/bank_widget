fc40c92b9ec3e65913cfb91a2a4d06bb02441b87
Райхана <rajhana@MacBook-Air-Rajhana.local>
2026-05-30 14:30:21 +0500

    Исправлены имена функций: get_mask_card_number и get_mask_account

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,19 @@
-def mask_card_number(number: str) -> str:
-    return f"{number[:4]} {number[4:6]}** **** {number[-4:]}"
+def get_mask_card_number(card_number: str) -> str:
+    """
+    Возвращает маску карты в формате XXXX XX** **** XXXX
+    
+    Пример: 7000792289606361 -> 7000 79** **** 6361
+    """
+    if not card_number or len(card_number) != 16:
+        return "Неверный номер карты"
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
-def mask_account_number(number: str) -> str:
-    return f"**{number[-4:]}"
+def get_mask_account(account_number: str) -> str:
+    """
+    Возвращает маску счета в формате **XXXX
+    
+    Пример: 73654108430135874305 -> **4305
+    """
+    if not account_number or len(account_number) < 4:
+        return "Неверный номер счета"
+    return f"**{account_number[-4:]}"
